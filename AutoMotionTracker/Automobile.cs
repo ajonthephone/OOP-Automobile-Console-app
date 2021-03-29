35c61e667bed06c17b2af117c7c7a9f80410f1c7
ajonthephone <ajonthephone@gmail.com>
2021-03-29 03:10:05 -0400

    Add files via upload

---
+++ AutoMotionTracker/Automobile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoMotionTracker
+{
+    public class Automobile
+    {
+        public string Car { get; set; }
+        public string Motion { get; set; }
+    }
+
+   
+}
---
+++ AutoMotionTracker/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AutoMotionTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Automobile myAuto = new Automobile();
+            string CarType = "";
+            Console.WriteLine("Type a vehicle manufacture, then enter");
+            CarType = Convert.ToString(Console.ReadLine());
+
+            if (CarType == "")
+            {
+                
+                myAuto.Car = "No Vehicle Selected";
+                myAuto.Motion = "Nothing is driving";
+            }
+            else {
+                myAuto.Car = CarType.ToString();
+                myAuto.Motion = "is driving";
+            }
+
+            //Automobile myAuto = new Automobile();
+            //myAuto.Car = "Honda";
+            //myAuto.Motion = "is driving";
+
+            Console.WriteLine(myAuto.Car.ToString() + " " + myAuto.Motion.ToString());
+
+           
+        }
+    }
+}
